f17f7903bb84cebb411ebb0adcb4c0fe903cca5d
Mario Anticoli <mario.anticoli@fcm.digital>
2023-08-24 15:21:41 +0200

    more extensive shell script

--- manage_k8.sh
+++ manage_k8.sh
@@ -1,33 +1,66 @@
 #!/bin/sh 
 
+IMAGE_LINE=17
+
 SKIP_CONTEXT_SELECTION=false
 USE_MICROK8S=true
 COMMAND=
+TAG=$(curl -s "https://hub.docker.com/v2/repositories/marioanticoli/idisclose/tags/"  | jq '.results[].name' | head -n 1 | tr -d '"')
+IMAGE=$(sed -n "${IMAGE_LINE}s/.*image: \([^[:space:]]*\).*/\1/p" k8s/web-deployment.yaml)
 
-for arg in "$@"; do
-  case $arg in
-    --skip-context-selection)
-      SKIP_CONTEXT_SELECTION=true
+while getopts ":adrb:cme:i:t:h" opt; do
+  case $opt in
+    a)
+      COMMAND=apply_all
+      ;; 
+    d)
+      COMMAND=delete_all
       ;;
-    --skip-microk8s)
-      USE_MICROK8S=false
+    r)
+      COMMAND=rollout
       ;;
-    --apply) 
-      COMMAND=apply 
+    b)
+      DEPLOYMENT=$OPTARG 
       ;;
-    --clean)
-      COMMAND=delete 
+    c) 
+      SKIP_CONTEXT_SELECTION=true 
+      ;; 
+    m) 
+      USE_MICROK8S=false 
       ;;
-    --help)
-      echo -e "--apply | --clean\t\tSelect the action to execute"
-      echo -e "--skip-context-selection\tSkip the context selection (use in scripts)"
-      echo -e "--skip-microk8s\t\t\tDon't use microk8s' kubectl"
+    e)
+      COMMAND=$OPTARG 
+      ;;
+    i)
+      IMAGE=$OPTARG
+      ;;
+    t)
+      TAG=$OPTARG
+      ;;
+    h)
+      echo -e "-a\tDeploy everything"
+      echo -e "-d\tDelete everything"
+      echo -e "-r\tRollout (requires to set -b deployment)"
+      echo -e "-b\tThe deployment name (only for rollout)"
+      echo -e "-c\tSkip the context selection (use in scripts)"
+      echo -e "-m\tDon't use microk8s' kubectl"
+      echo -e "-e\tA kubectl command to execute"
+      echo -e "-i\tAn image to pull instead of the default one"
+      echo -e "-t\tA tag instead of the most recent published (not \"latest\")"
+      echo -e "-h\tThis help"
       exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG"
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument."
+      ;;
   esac
 done
 
 if [ -z "$COMMAND" ]; then
-  echo "Error: COMMAND is not set. Please specify --apply or --clean."
+  echo "Error: COMMAND is not set. Please specify a kubectl command"
   exit 1
 fi
 
@@ -56,19 +89,19 @@ if ! $SKIP_CONTEXT_SELECTION; then
   kubectl config use-context $selected_context
 fi
 
-if [ "$COMMAND" = "apply" ]; then
-  TAG=$(curl -s "https://hub.docker.com/v2/repositories/marioanticoli/idisclose/tags/"  | jq '.results[].name' | head -n 1 | tr -d '"')
-
+if [ "$COMMAND" = "apply_all" ]; then
   kubectl apply -f k8s/prod-secret.yaml 
   kubectl apply -f k8s/db-persistent-volume.yaml
   kubectl apply -f k8s/db-persistent-volume-claim.yaml
   kubectl apply -f k8s/db-deployment.yaml
   kubectl apply -f k8s/db-service.yaml
-  sed "17s/$/:$TAG/" k8s/migration-job.yaml | kubectl apply -f -
-  sed "17s/$/:$TAG/" k8s/web-deployment.yaml | kubectl apply -f -
+  echo "Using $IMAGE:$TAG"
+  sed -e "${IMAGE_LINE}s|image: .*|image: $IMAGE|" -e "${IMAGE_LINE}s|$|:$TAG|" k8s/migration-job.yaml | kubectl apply -f -
+  sed -e "${IMAGE_LINE}s|image: .*|image: $IMAGE|" -e "${IMAGE_LINE}s|$|:$TAG|" k8s/web-deployment.yaml | kubectl apply -f -
   kubectl apply -f k8s/web-service.yaml
 
-elif [ "$COMMAND" = "delete" ]; then
+  kubectl get all
+elif [ "$COMMAND" = "delete_all" ]; then
   kubectl delete services idisclose-web-service
   kubectl delete services idisclose-db-service 
   kubectl delete deployments idisclose-web
@@ -77,7 +110,17 @@ elif [ "$COMMAND" = "delete" ]; then
   kubectl delete persistentvolume idisclose-postgres-data-persisent-volume 
   kubectl delete secret prod-secret
   kubectl delete jobs idisclose-migrations
+
+  kubectl get all
+elif [ "$COMMAND" = "rollout" ]; then
+  if [ -z "$DEPLOYMENT" ]; then
+    echo "Error: DEPLOYMENT is not set. Please specify a deployment."
+    exit 1
+  else
+    kubectl rollout restart deployment $DEPLOYMENT
+  fi
+else 
+  kubectl $COMMAND
 fi
 
-kubectl get all
 
